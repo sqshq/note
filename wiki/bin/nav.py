e7de6ebd8653d6ec99dfdb5337f3f60c1fb82934
Wang Zhenhua <wang.zhen.hua.larry@gmail.com>
2019-11-19 23:58:07 +0800

    nav sort by title

--- wiki/bin/nav.py
+++ wiki/bin/nav.py
@@ -9,18 +9,20 @@ import re
 
 # escape these directories when scanning
 INVALID_DIR = {'figures', 'custom_theme', 'tags', 'css', '爬虫', 'Mila', 'Prob',
-               'Projects', 'Java多线程', '其他', 'Leetcode', 'Miscellaneous', 'Tags', 'cpj', 'CSE521'}
+               'Projects', '其他', 'Tags', 'cpj', 'CSE521'}
 
 # top_categeory
-TOP_CATEGORY = {'Java', 'Algorithm', 'OS', 'Big Data'}
+TOP_CATEGORY = {'Java', 'Algorithm', 'OS', 'Big Data', 'Data Science', 'Miscellaneous', 'Leetcode'}
 
 # escape these files when scanning
-INVALID_FILES = {'index.md'}
+INVALID_FILES = {'index.md', '目录.md'}
 
 # 当前时间
-CURRTIME = "2017-07-07" 
-
+CURRTIME = "2017-07-07"
 CURRTIMES = time.strftime("%Y-%m-%d", time.localtime())
+
+# 配置文件
+CONFIG_FILE = "mkdocs.yml"
 
 # 文件开始格式
 FILEMETA = """---
@@ -31,144 +33,261 @@ date: %s
 
 """
 
+
 def get_wiki_site():
     cur_path = os.getcwd()
     if cur_path.endswith('/bin'):
         cur_path = cur_path[:-4]
     return cur_path
 
+
 def is_md_file(filename):
-	"""
-    Is file a markdown file?
     """
-	return filename[-3:] == '.md'
+    Is the file a markdown file?
+    """
+    return filename[-3:] == '.md'
+
+
+def is_hidden_file(filename):
+    """
+    return true if the given file is a hidden file
+    """
+    return filename.split("/")[-1].startswith(".")
 
 
 def is_valid_file(path):
-	"""
-	Return true if given file is either markdown file or a directory
-	"""
-	return is_md_file(path) or os.path.isdir(path)
+    """
+    Return true if the  given file is either markdown file or a valid directory
+    """
+    if is_hidden_file(path):
+        return False
+    return is_md_file(path) or os.path.isdir(path)
 
 
-def reindex(path):
-	"""
-	给每个文件夹生成一个导航文件
-	"""
-	# path对应的必须是个文件夹
-	if not os.path.isdir(path):
-		raise NameError("The path of Directory is INCORRECT.")
-	# 处理文件夹下面的每一个文件
-	chapters = []  # chapters, 文件名列表
-	books = []  # books
-	for filename in os.listdir(path):
-		file_path = os.path.join(path, filename)
-		# not a valid file
-		if not is_valid_file(file_path):
-			continue
-		# it is a directory
-		elif os.path.isdir(file_path) and filename not in INVALID_DIR:
-			book_content = reindex(file_path)
-			if book_content:  # valid content?
-				books.append((filename, book_content))
-		# it is a markdown file
-		elif is_md_file(file_path) and filename not in INVALID_FILES:
-			chapters.append(filename)
+def index(path):
+    """
+    给每个文件夹生成一个导航文件
+    """
 
-	# 生成一个导航文件
-	if books:  # 生成书的导航
-		index_books = create_index_books(books)
-		if path == os.path.join(get_wiki_site(), 'docs'): ## 这个是总目录
-			filename = '目录.md'   
-			index_books = replace_space(index_books)  #把路径中的空格替换
-		else:
-			filename = 'index.md'
-		write_index_books(os.path.join(path, filename), index_books)
-		return index_books
+    # path对应的必须是个文件夹
+    if not os.path.isdir(path):
+        raise NameError("The path of Directory is INCORRECT.")
+    # 处理文件夹下面的每一个文件
+    items = []  # items, 文件名列表
+    menus = []  # menus
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        # not a valid file
+        if not is_valid_file(file_path):
+            continue
+        # it is a directory
+        elif os.path.isdir(file_path) and filename not in INVALID_DIR:
+            x = index(file_path)
+            if x:  # valid content?
+                (menu_content, meta_menu) = x
+                menus.append((filename, menu_content, meta_menu))
+        # it is a markdown file
+        elif is_md_file(file_path) and filename not in INVALID_FILES:
+            items.append(filename)
 
-	elif chapters and len(chapters) > 3:  # 生成每本书的章节导航
-		index_chapters = create_index_chapters(chapters)
-		write_index_chapters(path, index_chapters)
-		return index_chapters
+    # 生成一个导航文件
+    if menus:  # 生成书的导航
+        index_menus = create_index_menus(menus)
+        meta_menuses = create_meta_menus(path, menus)
+        if path == os.path.join(get_wiki_site(), 'docs'):  # 这个是总目录
+            filename = '目录.md'
+            index_menus = replace_space(index_menus)  # 把路径中的空格替换
+            # 在配置文件mkdocs.yml中更新nav选项，以便生成正确的目录结构
+            update_mkdocs(os.path.join(os.path.split(path)[0], CONFIG_FILE), meta_menuses)
+        else:
+            # 这个是每本书的目录
+            filename = 'index.md'
+        write_index_menus(os.path.join(path, filename), index_menus)
+        return index_menus, meta_menuses
+    elif items and len(items) >= 3:  # 生成每本书的章节导航，但需要章节数大于3
+        index_items = create_index_items(items)
+        write_index_items(path, index_items)
+        # 产生每本书的元信息
+        meta_menu = create_meta_items(path, items)
+        return index_items, meta_menu
+
+
+class Nav:
+    """
+    导航：可以是一本书，或者一个章节
+    """
+    def __init__(self, title, location):
+        self.title = title
+        self.location = location
+
+
+class Item(Nav):
+    """
+    导航项目
+    """
+    def __str__(self):
+        return self.to_nav(0)
+
+    def to_nav(self, level):
+        """
+        选择第n级目录，并打印目录
+        """
+        return "    " * level + "- '" + self.title + "': '" + self.location + "'"
+
+
+class Menu(Nav):
+    """
+    导航菜单
+    """
+    def __init__(self, title, location, items):
+        super().__init__(title, location)
+        self.items = items
+
+    def __str__(self):
+        return self.to_nav(0)
+
+    def to_nav(self, level):
+        """
+        选择第n级目录，并打印目录
+        """
+        if isinstance(self.items[-1], Menu):  # 需要排序
+            self.items = sorted(self.items, key=lambda x: x.title)
+        rest = '\n'.join([item.to_nav(level + 1) for item in self.items])
+        if level < 0:   # 这一级目录跳过
+            return rest
+        return "    " * level + "- '" + self.title + "': " + "\n" + rest
+
+
+def update_mkdocs(path, meta_menuses):
+    """
+    在配置文件mkdocs.yml中更新nav选项
+    """
+    old_contents = []  # 列表每一项代表文件中的每一行
+    with codecs.open(path, mode='r', encoding='utf-8') as f:
+        old_contents = f.read()
+    # 寻找到nav标签，并且删除
+    try:
+        start_pos_of_nav = old_contents.index("nav:")
+    except:
+        start_pos_of_nav = len(old_contents)
+    contents = old_contents[0:start_pos_of_nav]
+    contents += ("nav:" + '\n')
+    new_contents = meta_menuses.to_nav(-1)
+    # 移除多余空格
+    with codecs.open(path, mode='w', encoding='utf-8') as f:
+        f.write(contents + new_contents)
+
 
 def replace_space(string):
-	"""
-	将路径中的空格替换成%20
-	"""
-	new_string = ""
-	for line in string.split('\n'):
-		match = re.search(r"\(.*\)", line)
-		if match:
-			revised = re.sub(r"\s", r"%20", match.group())
-			line = re.sub(r"\(.*\)", revised, line)
-		new_string += line + '\n'
-	return new_string
+    """
+    将路径中的空格替换成%20
+    """
+    new_string = ""
+    for line in string.split('\n'):
+        match = re.search(r"\(.*\)", line)
+        if match:
+            revised = re.sub(r"\s", r"%20", match.group())
+            line = re.sub(r"\(.*\)", revised, line)
+        new_string += line + '\n'
+    return new_string
 
 
-def write_index_books(path, contents):
-	"""
-	把书本/课程导航写入文件
-	"""
-	category = path.split(r'/')[-1]
-	title =  FILEMETA % (category, CURRTIME)
-	with codecs.open(path, mode='w', encoding='utf-8') as f:
-		f.write(title + contents)
+def write_index_menus(path, contents):
+    """
+    把书本/课程导航写入文件
+    """
+    category = path.split(r'/')[-1]
+    title = FILEMETA % ('Index', CURRTIME)
+    with codecs.open(path, mode='w', encoding='utf-8') as f:
+        f.write(title + contents)
 
 
-def create_index_books(books):
-	index_books = []
-	for book_name, book_content in books:
-		if book_name in TOP_CATEGORY:
-			title = '### %s \n\n' % book_name
-		else:
-			title = '#### %s \n\n' % book_name
-		index_books.append(title)
-		index_books.append(book_content.replace('(', '(' + book_name + '/'))
-	return ''.join(index_books)
+def create_index_menus(menus):
+    """
+    创建菜单的导航
+    """
+    index_menus = []
+    for menu_name, menu_content, meta_menu in menus:
+        if menu_name in TOP_CATEGORY:
+            title = '### %s \n\n' % menu_name
+        else:
+            title = '#### %s \n\n' % menu_name
+        index_menus.append(title)
+        index_menus.append(menu_content.replace('(', '(' + menu_name + '/'))
+    return ''.join(index_menus)
 
 
-def write_index_chapters(path, contents):
-	"""
-	把章节导航写入文件
-	"""
-	book = path.split(r'/')[-1]
-	title = FILEMETA % ("index", CURRTIME)
-	with codecs.open(os.path.join(path, 'index.md'), mode='w', encoding='utf-8') as f:
-		f.write(title + contents)
+def create_meta_menus(path, menus):
+    """
+    创建菜单的元信息
+    """
+    path = path.split("docs/")[-1]
+    menuses = []
+    for menu_name, menu_content, meta_menus in menus:
+        menuses.append(meta_menus)
+
+    meta_menus = Menu(path.split("/")[-1], path, menuses)
+    return meta_menus
 
 
-def create_index_chapters(chapters):
-	"""
-	返回写入到导航文件中的内容
-	chapters：章节列表，包含每一章名称以及路径
-	"""
-	index_chapters = []
-	chapters = sort_chapters(chapters)
-	for chapter_name in chapters:
-		index_chapters.append('* [%s](%s)\n' % (chapter_name.replace('.md', ''), chapter_name))
-	index_chapters.append('\n')
-	return ''.join(index_chapters)
+def write_index_items(path, contents):
+    """
+    把项目导航写入文件
+    """
+    menu = path.split(r'/')[-1]
+    title = FILEMETA % ("Index", CURRTIME)
+    with codecs.open(os.path.join(path, 'index.md'), mode='w', encoding='utf-8') as f:
+        f.write(title + contents)
 
 
-def sort_chapters(chapters):
-	"""
-	根据章节序号进行排序
-	"""
-	digit_sort = []
-	letter_sort = []
-	for chapter_name in chapters:
-		chapter_number = re.search(r'\d+', chapter_name)
-		# 如果章节号存在，则依据章节号排序
-		if chapter_number:
-			digit_sort.append((int(chapter_number.group()), chapter_name))
-		else:  # 否则根据字母排序
-			letter_sort.append((chapter_name, chapter_name))
-	letter_sort.sort()
-	digit_sort.sort()
-	# 字母在前面
-	letter_sort.extend(digit_sort)
-	return map(lambda x: x[1], letter_sort)
+def create_index_items(items):
+    """
+    返回写入到导航文件中的内容，用于生成index.md文件
+    items：项目列表，包含每一章名称以及路径
+    """
+    index_items = []
+    items = sort_items(items)
+    for item_name in items:
+        index_items.append('* [%s](%s)\n' % (item_name.replace('.md', ''), item_name))
+    index_items.append('\n')
+    return ''.join(index_items)
+
+
+def create_meta_items(path, items):
+    """
+    创建每本书的每个章节的元信息，用于生成左边的目录导航
+    items：章节列表，包含每一章名称以及相对路径
+    """
+    meta_items = []
+    items = sort_items(items)
+    path = path.split("docs/")[1]
+    for item_name in items:
+        name = item_name.replace('.md', '')
+        location = path + "/" + item_name
+        meta_items.append(Item(name, location))
+    meta_menu = Menu(path.split("/")[-1], path + "/index.md", meta_items)
+    return meta_menu
+
+
+def sort_items(items):
+    """
+    根据章节序号进行排序
+    """
+    digit_sort = []
+    letter_sort = []
+    for item_name in items:
+        item_number = re.search(r'\d+', item_name)
+        # 如果章节号存在，则依据章节号排序
+        if item_number:
+            digit_sort.append((int(item_number.group()), item_name))
+        else:  # 否则根据字母排序
+            letter_sort.append((item_name, item_name))
+    letter_sort.sort()
+    digit_sort.sort()
+    # 字母在前面
+    letter_sort.extend(digit_sort)
+    return map(lambda x: x[1], letter_sort)
 
 
 if __name__ == "__main__":
-	reindex(os.path.join(get_wiki_site(), 'docs'))
+    index(os.path.join(get_wiki_site(), 'docs'))
--- wiki/bin/parseHTMLTable.py
+++ wiki/bin/parseHTMLTable.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-import codecs
+from urllib.parse import urlparse
 import sys
 import os
+import codecs
+
 
 def isMd(filename):
     if filename[-3:] == '.md':
@@ -10,33 +12,64 @@ def isMd(filename):
     return False
 
 
-def parse_tables_from_html(url, md_file):
+def parse_tables_from_url(url, md_file):
+    """
+    parse tables from an url
+    convert it to a markdown(.md) file
+    """
+
+    r = requests.get(url)
+    parse_tables_from_html(r.text, md_file)
+
+
+def parse_tables_from_html(html, md_file):
     """
     parse tables from a html file
     convert it to a markdown(.md) file
     """
-        
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, features="lxml")
-    table_content = ""
+    soup = BeautifulSoup(html, features="lxml")
+    table_contents = ""
     for table in soup.select('table'):
-        table_content += process_table(table)
+        try:
+            table_content = process_table(table)
+            table_contents += table_content
+        except:
+            continue
+
+    if not table_contents:
+        print("NO VALID TABLE")
+        return
+
+    # write to the file
     with codecs.open(md_file, mode='w', encoding='utf-8') as file:
-        file.write(table_content)
+        file.write(table_contents)
+    print("The Table is saved in" + md_file)
+
 
 def process_table(table):
     tableMd = ""
-    thead = table.select_one('thead')
     tbody = table.select_one('tbody')
-    tfoot = table.select_one('tfoot')
-    num_of_columns = len(thead.select('th'))
+    thead = table.select_one('thead')
+    if not tbody:
+        tbody = table
+        thead = table.select_one('tr')
+    if not thead:
+        thead = table.select_one('tr')
+    if not thead:
+        thead = table.select_one('td')
+
     # processing headerline
     header_items = thead.select('th')
-    headerlineMd = '| ' # add headerline
+    if not header_items:
+        header_items = thead.select('tr')
+    if not header_items:
+        header_items = thead.select('td')
+    num_of_columns = len(header_items)
+    headerlineMd = '| '  # add headerline
     for th in header_items:
-        headerlineMd += th.text + " | "
+        headerlineMd += th.text.replace("\n", " ") + " | "
     tableMd += headerlineMd + '\n'
-    
+
     # add line mark
     line_mark = '|'
     for i in range(num_of_columns):
@@ -47,23 +80,58 @@ def process_table(table):
     for row in tbody.select('tr'):
         rowMd = "| "
         for cell in row.select('td'):
-            rowMd += cell.text + ' | '
+            rowMd += cell.text.replace("\n", " ").replace("\t", " ") + ' | '
         tableMd += rowMd + '\n'
     return tableMd + '\n'
+
+
+def is_url(url):
+    """
+    判断给定字符串是不是URL
+    :param url: 字符串
+    :return: URL返回True，否则返回False
+    """
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def is_html(filename):
+    """
+    判断文件是不是HTML文件
+    :param file: 文件
+    :return: HTML文件返回True，否则False
+    """
+    try:
+        if filename[-5:] == ".html" or filename[-4:] == ".htm":
+            return True
+    except:
+        return False
+    return False
 
 if __name__ == '__main__':
     len_of_argv = len(sys.argv)
     if len_of_argv < 2:
-        print('Usage: python parseHTMLTable.py url [markdown-file]')
+        print('Usage: python parseHTMLTable.py url | html-file  [markdown-file]')
         exit()
     elif len_of_argv == 2:
-        url = sys.argv[1]
-        parse_tables_from_html(url, 'sample.md')
-        print("Table is saved in sample.md.")
+        url_or_file = sys.argv[1]
+        if is_url(url_or_file):
+            url = url_or_file
+            parse_tables_from_url(url, 'sample.md')
+        elif is_html(url_or_file):
+            htmlfile = os.path.join(os.curdir, url_or_file)
+            with codecs.open(htmlfile, mode='r', encoding='utf-8') as file:
+                html = file.read()
+            parse_tables_from_html(html, 'sample.md')
+        else:
+            print('Usage: python parseHTMLTable.py url | html-file  [markdown-file]')
+            exit()
+
     elif len_of_argv == 3:
         url = sys.argv[1]
         md_file = sys.argv[2]
-        parse_tables_from_html(url, md_file)
-        print("Table is saved in " + md_file)
-
-
+        parse_tables_from_url(url, md_file)
+        print("The Table is saved in " + md_file)
